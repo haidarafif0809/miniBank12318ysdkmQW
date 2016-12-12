fb63da5aedfa24415630bc4717b6f822db6b4867
hendriandaglos <baifernsh25>
2016-12-12 12:01:44 +0700

    COMMIT AWAL

---
+++ db.php
@@ -0,0 +1,15 @@
+
+<?php
+
+date_default_timezone_set("Asia/Jakarta");
+
+// perintah untuk mengkoneksikan php ke database mysql
+$db = new mysqli('localhost','root','','sekolah');
+
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+
+
+?>
